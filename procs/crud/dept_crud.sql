6dd4aacbca02c69a6cbe81e800a6eb83ba3ec207
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-04-07 06:35:28 +0200

    Track Table CRUD Procedures

--- procs/crud/dept_crud.sql
+++ procs/crud/dept_crud.sql
@@ -114,6 +114,140 @@ BEGIN
 END;
 $$;
 
+--================================================================================================================================================
 
 
+--Track Table CRUD Procedures:
 
+--=========================================
+--procedure Name: InsertTrack
+--Description: Adds new track
+--Parameters:
+--		p_TrackName : track name
+--		p_DepartmentName : department name
+--=========================================
+
+CREATE OR REPLACE PROCEDURE InsertTrack(
+    p_TrackName TEXT,
+    p_DepartmentName TEXT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_DepartmentID INT;
+BEGIN
+    SELECT DepartmentID
+    INTO v_DepartmentID
+    FROM Department
+    WHERE DepartmentName = p_DepartmentName;
+
+    INSERT INTO Track (TrackName, DepartmentID)
+    VALUES (p_TrackName, v_DepartmentID);
+END;
+$$;
+
+
+--=========================================
+--procedure Name: UpdateTrack
+--Description: updates existing track name
+--Parameters:
+--		p_oldTrackName : old track name
+--		p_newTrackName : updated track name
+--		p_DepartmentName : department name
+--=========================================
+
+CREATE OR REPLACE PROCEDURE UpdateTrack(
+	p_oldTrackName TEXT,
+	p_newTrackName TEXT,
+	p_DepartmentName TEXT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	v_DepartmentID INT;
+BEGIN
+    SELECT DepartmentID
+    INTO v_DepartmentID
+    FROM Department
+    WHERE DepartmentName = p_DepartmentName;
+
+	IF v_DepartmentID IS NULL THEN
+    	RAISE EXCEPTION 'Department not found';
+	END IF;
+
+	UPDATE Track
+	SET
+		TrackName = p_newTrackName,
+		DepartmentID = v_DepartmentID
+	WHERE TrackName = p_oldTrackName;
+END;
+$$;
+
+
+--=========================================
+--procedure Name: DeleteTrackByName
+--Description: Deletes existing track
+--Parameters:
+--		p_TrackName : track name
+--=========================================
+
+CREATE OR REPLACE PROCEDURE DeleteTrackByName(
+	p_TrackName TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	DELETE FROM Track
+	WHERE TrackName = p_TrackName;
+END;
+$$;
+
+
+--=========================================
+--function Name: SelectTrackByName
+--Description: Returns track by track name
+--Parameters:
+--		TrackName : track name
+--		DepartmentName : department name
+-- call example : SELECT * FROM SelectTrackByName('trackName');
+--=========================================
+
+CREATE OR REPLACE FUNCTION SelectTrackByName(
+	p_TrackName TEXT
+)
+RETURNS TABLE (
+    TrackName TEXT,
+    DepartmentName TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	RETURN QUERY
+	SELECT t.TrackName, d.DepartmentName
+	FROM Track t
+	JOIN Department d ON d.DepartmentID = t.DepartmentID
+	WHERE t.TrackName = p_TrackName;
+END;
+$$;
+
+
+--=========================================
+--function Name: SelectAllTracks
+--Description: Returns all tracks
+-- call example : SELECT * FROM SelectAllTracks()
+--=========================================
+
+CREATE OR REPLACE FUNCTION SelectAllTracks()
+RETURNS TABLE (
+    TrackName TEXT,
+    DepartmentName TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT t.TrackName, d.DepartmentName
+    FROM Track t
+    JOIN Department d ON d.DepartmentID = t.DepartmentID;
+END;
+$$;
