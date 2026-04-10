708d93cb3972bf1c6167e2b3463d550d56d64312
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-04-10 21:25:31 +0200

    solve minor uncaught errors in question_crud  and dept_crud

---
+++ tests/Main testing/Test.sql
@@ -0,0 +1,17 @@
+
+--================================
+-- Drop public schema
+--================================
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+--================================
+-- Restore default permissions
+--================================
+
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+
+--==================================
+
+ROLLBACK
